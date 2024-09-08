d1081333465ff0f333d52494f6de030cc8605ac7
Yurzs <yurzs@icloud.com>
2024-09-08 18:39:18 +0700

    entrypoint: remove trash

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ if ! test -f /etc/letsencrypt/dummy.pem; then
   openssl genrsa -out dummy.key 2048 >/dev/null 2>&1
   openssl req -new -key dummy.key -out dummy.csr -subj "/C=''/L=''/O=''/CN=''" >/dev/null 2>&1
   openssl x509 -req -days 3650 -in dummy.csr -signkey dummy.key -out dummy.crt >/dev/null 2>&1
-  mv dummy.crt /etc/letsencrypt/dummy.pem
-  rm dummy.key dummy.csr
+  cat dummy.crt dummy.key > /etc/letsencrypt/dummy.pem
+  rm dummy.key dummy.csr dummy.crt
 fi
 
 exec "$@"
