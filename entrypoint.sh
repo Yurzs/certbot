843ff0b62d08f74fb5e3230bd4891a2a2039bf3a
Yurzs <yurzs@icloud.com>
2024-09-08 18:38:51 +0700

    entrypoint: move cert

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ if ! test -f /etc/letsencrypt/dummy.pem; then
   openssl genrsa -out dummy.key 2048 >/dev/null 2>&1
   openssl req -new -key dummy.key -out dummy.csr -subj "/C=''/L=''/O=''/CN=''" >/dev/null 2>&1
   openssl x509 -req -days 3650 -in dummy.csr -signkey dummy.key -out dummy.crt >/dev/null 2>&1
+  mv dummy.crt /etc/letsencrypt/dummy.pem
+  rm dummy.key dummy.csr
 fi
 
 exec "$@"
